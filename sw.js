62e4b74d5a5a6450257d99e8625524a3c73e50b2
victory-zzm <2289677921@qq.com>
2021-05-11 21:04:58 +0800

    Site updated: 2021-05-11 21:04:58

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/sw-register.js","4c9cd8db13d01303767bf82d2b535946"]];
+var precacheConfig = [];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
